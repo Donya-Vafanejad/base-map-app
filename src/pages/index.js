1c585bce5d57cf34cba661d9a332478cc46d1611
Donya-Vafanejad <alicedonya988@gmail.com>
2023-03-11 16:26:01 -0800

    connecting to git

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Map from 'react-map-gl'
+import DeckGL from '@deck.gl/react';
+import { NewYorkTreesLayer } from '../layers/scatterplot/new-york-trees'
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoieXV5YWZ1amltb3RvIiwiYSI6ImNsZWdyZDA4NTA1ZGwzeG53Y2c2OGY0bjAifQ._mbKMYj_moCfX1GcJmxAJg'
-
 
 export default function Home() {
   return (
@@ -16,16 +16,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <Map 
-          initialViewState={{
-            longitude: -122.4,
-            latitude: 37.8,
-            zoom: 14
-          }}
-          style={{width: '100vw', height: '100vh'}}
-          mapStyle="mapbox://styles/mapbox/dark-v9"
-          mapboxAccessToken={MAPBOX_TOKEN}
-        />
+        <DeckGL
+          initialViewState={NewYorkTreesLayer.initialViewState}
+          controller={true}
+          layers={[NewYorkTreesLayer.layer]}
+        >
+          <Map
+            style={{width: '100vw', height: '100vh'}}
+            mapStyle="mapbox://sprites/mapbox/dark-v8"
+            mapboxAccessToken={MAPBOX_TOKEN}
+          />
+        </DeckGL>
       </main>
     </>
   )
